da8b73a9969030cc3921ece46ae0a39c51d2a160
Evan Marcey <emarcey@cbinsights.com>
2018-11-09 12:37:17 -0500

    Updated regex

--- src/config.py
+++ src/config.py
@@ -13,7 +13,10 @@ SLACK_CLIENT = SlackClient(BOT_ACCESS_TOKEN)
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 KARMA_TIMEOUT = 30
-KARMA_REGEX = re.compile(r"(?:^| )\<\@([\S]+)\> ?([\+\-]+)")
+# Looks for <@user_id> +/- string
+# Looks ahead to make sure, either message terminates or is followed by another karma group
+# If not, does not parse as karma
+KARMA_REGEX = re.compile(r"(?:^| )\<\@([\S]+)\> ?([\+\-]+)(?=[ ]*$|[ ]?(?=\<\@(?:[\S]+)\> ?([\+\-]+)))")
 
 KARMA_URL = os.environ.get('KARMA_URL')
 
@@ -22,3 +25,8 @@ BUZZKILL = 100
 
 # TODO: This is a channel ID for a
 KARMABOT_ADMIN_CHANNEL = 'GDZ9KH17A'
+
+
+KARMA_API_KEY = os.environ.get('KARMA_API_KEY')
+if not KARMA_API_KEY:
+    raise Exception("No API key found! Can't connect to the backend!")
